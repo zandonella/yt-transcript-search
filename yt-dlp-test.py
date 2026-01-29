4d6378156616c55843a44e0599ae9b4135c6aa74
Michael Zandonella <123271994+Zando411@users.noreply.github.com>
2026-01-29 10:18:06 -0800

    add working examples with yp-dlp

--- yt-dlp-test.py
+++ yt-dlp-test.py
@@ -15,9 +15,9 @@ def download_transcripts_for_videos(playlist_url):
         'writesubtitles': True,
         'writeautomaticsub': True,
         "subtitleslangs": ["en"],
-        'subtitlesformat': 'json3',
+        'subtitlesformat': 'vtt',
 
-        'outtmpl': str(out_path / "%(playlist_title)s/%(title)s [%(id)s].%(ext)s"),
+        'outtmpl': str(out_path / "%(uploader)s - %(playlist_title)s/%(title)s [%(id)s].%(ext)s"),
 
         "yesplaylist": True,
 
@@ -34,6 +34,6 @@ def download_transcripts_for_videos(playlist_url):
 
 
 if __name__ == "__main__":
-    playlist_url = "https://www.youtube.com/playlist?list=PLlDdLy-1g17ZQlKROSatxk7uGovsBwQ8p"
+    playlist_url = input("Enter the YouTube playlist URL: ").strip()
     stats = download_transcripts_for_videos(playlist_url)
     print("Download complete.")
